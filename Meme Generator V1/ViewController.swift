442a42b815b76ef0ca16a275fa512257b1ae14b6
Markus Staas <markus@Markuss-Mac.local>
2017-03-06 14:00:04 +0700

    Initial Commit

---
+++ Meme Generator V1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme Generator V1
+//
+//  Created by Markus Staas on 3/6/17.
+//  Copyright © 2017 Markus Staas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
